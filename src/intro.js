3d743f56360a806673b5bf6f49e418e0713e3cc9
Ismail ZAHIR <ismailza407@gmail.com>
2024-06-16 16:56:08 +0100

    use TDD for the factorial function

--- src/intro.js
+++ src/intro.js
@@ -16,3 +16,9 @@ export function calculateAverage(numbers) {
   if (numbers.length === 0) return NaN;
   return numbers.reduce((acc, n) => acc + n, 0) / numbers.length;
 }
+
+// Calculate the factorial of a number
+export function factorial(n) {
+  if (n < 0) return undefined;
+  return n === 0 || n === 1 ? 1 : n * factorial(n - 1);
+}
--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,6 +1,6 @@
 import { describe, test, it, expect } from "vitest";
 
-import { max, fizzBuzz, calculateAverage } from "../src/intro";
+import { max, fizzBuzz, calculateAverage, factorial } from "../src/intro";
 
 describe('max test', () => {
   it('should return the first argument if it is greater', () => {
@@ -49,5 +49,31 @@ describe('calculate average', () => {
 
   it('should calculate the average of an array with multiple elements', () => {
     expect(calculateAverage([1, 2, 3, 4, 5])).toBe(3);
+  });
+});
+
+describe('factorial test', () => {
+  it ('should return 1 if the number given is 0', () => {
+    expect(factorial(0)).toBe(1);
+  });
+
+  it('should return undefined if the number given is negative', () => {
+    expect(factorial(-1)).toBeUndefined();
+  });
+
+  it('should return 1 if given 1', () => {
+    expect(factorial(1)).toBe(1);
+  });
+
+  it('should return 2 if given 2', () => {
+    expect(factorial(2)).toBe(2);
+  });
+
+  it('should return 6 if given 3', () => {
+    expect(factorial(3)).toBe(6);
+  });
+
+  it('should return 120 if given 5', () => {
+    expect(factorial(5)).toBe(120);
   });
 });
