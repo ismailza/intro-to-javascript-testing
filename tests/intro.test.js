2e7e4ea667be10ecff45dc66f53752861a34d9dc
Ismail ZAHIR <ismailza407@gmail.com>
2024-06-16 15:58:59 +0100

    create a first test for max function

---
+++ tests/intro.test.js
@@ -0,0 +1,17 @@
+import { describe, test, it, expect } from "vitest";
+
+import { max } from "../src/intro";
+
+describe('max test', () => {
+  it('should return the first argument if it is greater', () => {
+    expect(max(2, 1)).toBe(2);
+  });
+
+  it('should return the second argument if it is greater', () => {
+    expect(max(1, 2)).toBe(2);
+  });
+
+  it('should return the first argument if they are equal', () => {
+    expect(max(2, 2)).toBe(2);
+  });
+});
